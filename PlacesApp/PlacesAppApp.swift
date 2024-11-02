57a5c64c434f51db6ef853826ec96fbf57a11239
Mahmoud Abdelhady <eng.mahmoud.sh.77@gmail.com>
2024-11-02 15:24:38 +0200

    Initial Commit

---
+++ PlacesApp/PlacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAppApp.swift
+//  PlacesApp
+//
+//  Created by Mahmoud Elsharkawy on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
